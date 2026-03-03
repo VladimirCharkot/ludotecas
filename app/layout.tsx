8904e628c46f6f035fca2b774a9d53a1bd0dda32
Vlad <vlad.chk@gmail.com>
2026-03-03 11:58:55 -0300

    meta: opengraph y loco

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,21 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Ludotecas, Ajedrez y Go",
   description: "Programa Ludotecas, Ajedrez y Go de la Subdirección de Participación, Derechos y Comunidad del Ministerio de Educación de la Provincia de Córdoba",
+  openGraph: {
+    title: 'Ludotecas, Ajedrez y Go',
+    description: 'Programa Ludotecas, Ajedrez y Go de la Subdirección de Participación, Derechos y Comunidad del Ministerio de Educación de la Provincia de Córdoba',
+    url: 'https://ludotecascba.vercel.app',
+    siteName: 'Ludotecas, Ajedrez y Go',
+    images: [
+      {
+        url: 'https://ludotecascba.vercel.app/logo_ludotecas.svg',
+        width: 480,
+        height: 480,
+      },
+    ],
+    locale: 'es_AR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
