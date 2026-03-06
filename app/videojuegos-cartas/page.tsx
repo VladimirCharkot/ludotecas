84c930d09f02bab0bfa104f82ed3a396dbcc63a6
Vlad <vlad.chk@gmail.com>
2026-03-06 11:34:24 -0300

    link: videojuegos cartas

---
+++ app/videojuegos-cartas/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function VideojuegosCartas() {
+  redirect("https://docs.google.com/document/d/1dcU0J_cHolHb7EOvM2mabx0pfqff1Z4owqzyUPsfd7o/");
+}
