eb8b4378678b5dedb8f3d6fe092e2d1cf0593981
Vlad <vlad.chk@gmail.com>
2026-04-24 09:54:41 -0300

    Mails en portada

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,20 @@
 import LinksPortada from "@/components/links-svg"
 
 export default function Home() {
-  return <LinksPortada />
+  return (
+    <>
+      <LinksPortada />
+      <div className="max-w-lg mx-auto mb-8 text-center">
+        <p className="font-barriecito mb-4">Contactos:</p>
+
+        <p className="font-semibold">
+          Línea Ludotecas y dispositivos lúdicos:{" "}
+        </p>
+        <pre>ludotecaseducacion.cba@gmail.com</pre>
+
+        <p className="font-semibold mt-6">Línea Ajedrez educativo: </p>
+        <pre>ajedrezeducativocba@gmail.com</pre>
+      </div>
+    </>
+  )
 }
